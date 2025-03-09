72e38f805e33ae462e2f318dc8e8971362f7cd1d
Neeraj Kumar <nk35715945@gmail.com>
2025-03-09 20:50:20 +0530

    creating the Tab bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,18 @@
 import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+
+    screenOptions={{
+      tabBarActiveTintColor: '#4A90E2', // Active tab icon color
+      tabBarInactiveTintColor: '#b0b0b0', // Inactive tab icon color
+      headerShown: false, // Hide header
+     
+    }}
+    >
+      <Tabs.Screen name="index" options={{ title: 'Training' }} />
+      <Tabs.Screen name="Setting" options={{ title: 'Setting' }} />
     </Tabs>
   );
 }
